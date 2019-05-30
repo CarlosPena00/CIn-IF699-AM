07b69b4f9d191c0181ff729611d7484385c61283
CarlosPena00 <chcp@cin.ufpe.br>
2019-05-30 20:55:49 -0300

    Add example for balance dataset

--- data/download.sh
+++ data/download.sh
@@ -1,2 +1,4 @@
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.names
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.data
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.names
