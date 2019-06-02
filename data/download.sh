b3d84298b3d0dea4142055627770e6baea80a839
Matheus Casa Nova da Luz <mcnl@cin.ufpe.br>
2019-06-02 11:15:50 -0300

    Add files via upload
    Add dataset treatment (normalization, categorical values and others) to some on the dataset. Add all dataset to download.sh file.

--- data/download.sh
+++ data/download.sh
@@ -4,3 +4,15 @@ wget http://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/bala
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.names
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/blood-transfusion/transfusion.data
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/blood-transfusion/transfusion.names
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.names
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.names
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/australian/australian.dat
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/australian/australian.doc
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/australian/australian.dat
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/australian/australian.doc
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00252/pop_failures.dat
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.doc
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00342/Data_Cortex_Nuclear.xls
